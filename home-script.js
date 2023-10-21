36b9b60f3c9df3cd50774c959c848c6630c179a3
gabrielglezam <gabrielglezam@gmail.com>
2023-10-21 10:26:06 -0600

    Update home-script.js

--- home-script.js
+++ home-script.js
@@ -651,3 +651,50 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+// Handle group clicks
+const groups = document.querySelectorAll(".continent-group");
+
+groups.forEach((group) => {
+  group.addEventListener("click", function () {
+    // Toggle selected class for all paths within the clicked group
+    this.querySelectorAll("path").forEach((path) => {
+      path.classList.toggle("selected");
+    });
+  });
+});
+
+
+// Your existing jQuery code
+$(".map__continent").hover(function () {
+  $(this)
+    .siblings(".map__continent")
+    .find(".continent")
+    .toggleClass("sibling-continent");
+});
+
+$(".dot-embed").mouseenter(function () {
+  $(".location").text($(this).attr("continent-name"));
+});
+
+$(".dot-embed").mouseleave(function () {
+  $(".location").text("SELECT A CONTINENT");
+});
+
+$(".pop-item").css("display", "none");
+$(".pop-item").eq(0).css("display", "block");
+
+$(".item-dot").click(function () {
+  var currentNumber = $(this).closest(".item-dot").index();
+  $(".pop-item").css("display", "none");
+  $(".pop-item").eq(currentNumber).css("display", "block");
+});
+
+//open popup
+$(".dot-embed").on("click", function () {
+  $(".pop-wrap").addClass("is--show");
+});
+
+$(".item-link").on("click", function () {
+  $(".pop-wrap").removeClass("is--show");
+});
