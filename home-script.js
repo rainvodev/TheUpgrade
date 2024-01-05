16cfa09efbd5d68760c9fc44c8afc65b3153be48
gabrielglezam <gabrielglezam@gmail.com>
2024-01-05 10:06:56 -0600

    Update home-script.js

--- home-script.js
+++ home-script.js
@@ -3,49 +3,7 @@
     types: "words, chars",
     tagName: "span"
   });
-  //LENIS SCROLL
-  ("use strict"); // fix lenis in safari
-
-  if (Webflow.env("editor") === undefined) {
-    const lenis = new Lenis({
-      lerp: 0.1,
-      wheelMultiplier: 0.5,
-      infinite: false,
-      gestureOrientation: "vertical",
-      normalizeWheel: false,
-      smoothTouch: false
-    });
-
-    function raf(time) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-    requestAnimationFrame(raf);
-
-    $("[data-lenis-start]").on("click", function () {
-      lenis.start();
-    });
-    $("[data-lenis-stop]").on("click", function () {
-      lenis.stop();
-    });
-    $("[data-lenis-toggle]").on("click", function () {
-      $(this).toggleClass("stop-scroll");
-      if ($(this).hasClass("stop-scroll")) {
-        lenis.stop();
-      } else {
-        lenis.start();
-      }
-    });
-
-    function connectToScrollTrigger() {
-      lenis.on("scroll", ScrollTrigger.update);
-      gsap.ticker.add((time) => {
-        lenis.raf(time * 1000);
-      });
-    }
-    // Uncomment this if using GSAP ScrollTrigger
-    // connectToScrollTrigger();
-  }
+  
   //GSAP
   //PreLoader
   function pageLoad() {
